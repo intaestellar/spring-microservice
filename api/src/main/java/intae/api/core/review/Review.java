cc75391092865fc957dcc54a21ce39e61f40c5a6
intaeHwang <hwang9029@ckinform.com>
2026-09-14 12:29:10 +0900

    api.core 추가

---
+++ api/src/main/java/intae/api/core/review/Review.java
@@ -0,0 +1,15 @@
+package intae.api.core.review;
+
+import lombok.AllArgsConstructor;import lombok.Getter;import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+public class Review {
+    private final int productId;
+    private final int reviewId;
+    private final String author;
+    private final String subject;
+    private final String content;
+    private final String serviceAddress;
+}
