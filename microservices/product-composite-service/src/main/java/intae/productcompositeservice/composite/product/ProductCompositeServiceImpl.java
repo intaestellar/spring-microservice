c420d508bdd76f2697ede806ba5075403ef76679
intaeHwang <hwang9029@ckinform.com>
2026-09-16 11:00:55 +0900

    복합 API 구현

---
+++ microservices/product-composite-service/src/main/java/intae/productcompositeservice/composite/product/ProductCompositeServiceImpl.java
@@ -0,0 +1,69 @@
+package intae.productcompositeservice.composite.product;
+
+import intae.api.composite.product.*;
+import intae.api.core.product.Product;
+import intae.api.core.recommendation.Recommendation;
+import intae.api.core.review.Review;
+import intae.api.exceptions.NotFoundException;
+import intae.util.http.ServiceUtil;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductCompositeServiceImpl implements ProductCompositeService {
+
+    private final ServiceUtil serviceUtil;
+    private final ProductCompositeIntegration integration;
+
+    public ProductCompositeServiceImpl(
+            ServiceUtil serviceUtil,
+            ProductCompositeIntegration integration
+    ) {
+        this.serviceUtil = serviceUtil;
+        this.integration = integration;
+    }
+
+    @Override
+    public ProductAggregate getProduct(int productId) {
+
+        Product product = integration.getProduct(productId);
+        if (product == null) {
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+
+        List<Recommendation> recommendations = integration.getRecommendations(productId);
+
+        List<Review> reviews = integration.getReviews(productId);
+
+        return createProductAggregate(product, recommendations, reviews, serviceUtil.getServiceAddress());
+    }
+
+    private ProductAggregate createProductAggregate(
+            Product product,
+            List<Recommendation> recommendations,
+            List<Review> reviews,
+            String serviceAddress
+    ) {
+        int productId = product.getProductId();
+        String name = product.getName();
+        int weight = product.getWeight();
+
+        List<RecommendationSummary> recommendationSummaries =
+                (recommendations == null) ? null : recommendations.stream()
+                        .map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate()))
+                        .toList();
+
+        List<ReviewSummary> reviewSummaries =
+                (reviews == null) ? null : reviews.stream()
+                        .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+                        .toList();
+
+        String productAddress = product.getServiceAddress();
+        String reviewAddress = (reviews != null && !reviews.isEmpty()) ? reviews.getFirst().getServiceAddress() : "";
+        String recommendationAddress = (recommendations != null && !recommendations.isEmpty()) ? recommendations.getFirst().getServiceAddress() : "";
+        ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress, reviewAddress, recommendationAddress);
+
+        return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries, serviceAddresses);
+    }
+}
