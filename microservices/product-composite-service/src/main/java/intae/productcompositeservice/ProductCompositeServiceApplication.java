02b79e80281655dab2690d9f113bbd7866db74b2
intaeHwang <hwang9029@ckinform.com>
2026-09-22 11:59:08 +0900

    swagger 적용

--- microservices/product-composite-service/src/main/java/intae/productcompositeservice/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/intae/productcompositeservice/ProductCompositeServiceApplication.java
@@ -1,5 +1,11 @@
 package intae.productcompositeservice;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +16,49 @@ import org.springframework.web.client.RestTemplate;
 @ComponentScan("intae")
 public class ProductCompositeServiceApplication {
 
+    @Value("${api.common.version}")         String apiVersion;
+    @Value("${api.common.title}")           String apiTitle;
+    @Value("${api.common.description}")     String apiDescription;
+    @Value("${api.common.termsOfService}")  String apiTermsOfService;
+    @Value("${api.common.license}")         String apiLicense;
+    @Value("${api.common.licenseUrl}")      String apiLicenseUrl;
+    @Value("${api.common.externalDocDesc}") String apiExternalDocDesc;
+    @Value("${api.common.externalDocUrl}")  String apiExternalDocUrl;
+    @Value("${api.common.contact.name}")    String apiContactName;
+    @Value("${api.common.contact.url}")     String apiContactUrl;
+    @Value("${api.common.contact.email}")   String apiContactEmail;
+
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public OpenAPI getOpenApiDocumentation() {
+        ExternalDocumentation externalDocs = new ExternalDocumentation()
+                .description(apiExternalDocDesc)
+                .url(apiExternalDocUrl);
+
+        License license = new License()
+                .name(apiLicense)
+                .url(apiLicenseUrl);
+
+        Contact contact = new Contact()
+                .name(apiContactName)
+                .url(apiContactUrl)
+                .email(apiContactEmail);
+
+        Info info = new Info().title(apiTitle)
+                .description(apiDescription)
+                .version(apiVersion)
+                .contact(contact)
+                .termsOfService(apiTermsOfService)
+                .license(license);
+
+        return new OpenAPI().info(info)
+                .externalDocs(externalDocs);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ProductCompositeServiceApplication.class, args);
